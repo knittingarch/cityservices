8e4c6312a67a650fe2105b88227f62cb0dfeef5f
Sarah Dawson <knittingarch@gmail.com>
2018-02-01 09:18:11 -0500

    Switch Dbs and Re-run Migrations; Attempt Writing Preliminary Tests

---
+++ tests/streets/test_street_view.py
@@ -0,0 +1,13 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class TestStreetView(APITestCase):
+    @pytest.mark.django_db
+    def test_returns_street_list(self):
+        url = reverse('')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        
