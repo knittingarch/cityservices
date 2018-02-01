8e0c61d54dcd399265b8ac28c1f7a2a980a86fe6
Sarah Dawson <knittingarch@gmail.com>
2018-02-01 13:07:18 -0500

    Clean Up Code
    This commit removes the comments from the settings, and adds one to the attempted test.

--- streetsweeper/settings.py
+++ streetsweeper/settings.py
@@ -84,8 +84,6 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
