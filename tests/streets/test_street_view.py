08a49fbb154d0b8fdbbc255d35933eb27f16b4f9
Sarah Dawson <knittingarch@gmail.com>
2018-02-05 04:56:16 -0500

    Fix Stickler Complaint

--- tests/streets/test_street_view.py
+++ tests/streets/test_street_view.py
@@ -7,7 +7,7 @@ from rest_framework.test import APITestCase
 class TestStreetView(APITestCase):
     @pytest.mark.django_db
     def test_returns_street_list(self):
-        # Need to figure out how to test view sets... 
+        # Need to figure out how to test view sets...
         url = reverse('')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
