79da70b3ef0d0c4cb288d7966d9101e29259a0a7
Sarah Dawson <knittingarch@gmail.com>
2018-04-07 08:47:51 -0400

    Update Pagination To Ten Results

--- streetsweeper/settings.py
+++ streetsweeper/settings.py
@@ -91,9 +91,11 @@ db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
 # Pagination
+#restrict access to endpoints in production
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 # Password validation
